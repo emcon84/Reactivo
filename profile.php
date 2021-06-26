06131d47fdfab285898d62fbf25e6479db9bd7a9
Emiliano Conti <emcon84@gmail.com>
2021-06-25 22:14:39 -0300

    first

--- profile.php
+++ profile.php
@@ -18,8 +18,9 @@
                       <h4>Emiliano FrontDev</h4>
                       <p class="text-secondary mb-1">Frontend Devoloper</p>
                       <p class="text-muted font-size-sm">Reconquista Santa Fe</p>
-                      <button class="btn btn-primary">Seguir</button>
-                      <button class="btn btn-outline-primary">Mensaje</button>
+                      <button class="btn btn-warning">Seguir</button>
+                      <a class="btn btn-outline-warning" href="https://wa.me/message/7CDZOM6NGRU7J1">Mensaje</a>
+                      
                     </div>
                   </div>
                 </div>
@@ -29,17 +30,17 @@
                   
                   <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                    <span class="text-secondary">emcon84</span>
+                   <a href="https://github.com/emcon84"> <span class="text-secondary">emcon84</span></a>
                   </li>
                   
                   <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin mr-2 icon-inline">
                     <circle cx="4" cy="5" r="2"></circle><rect x="2" y="10.25" width="4" height="11.75"></rect><path d="M17.3837 10c-1.8723 0-3.1275.9705-3.6414 1.891h-.0515v-1.6H10V22h3.8448v-5.7923c0-1.527.3074-3.0062 2.3097-3.0062 1.9735 0 1.9998 1.7467 1.9998 3.1043V22H22v-6.4224C22 12.4252 21.2806 10 17.3837 10"/>
-                  </svg></h6><span class="text-secondary"> emiliano-conti</span>
+                  </svg></h6><a href="https://www.linkedin.com/in/emiliano-conti/"> <span class="text-secondary">Emiliano Conti</span></a>
                   </li>
                   <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                    <span class="text-secondary">ReactivoSD</span>
+                    <a href="https://www.facebook.com/ReactivoSD"> <span class="text-secondary">ReactivoSD</span></a>
                   </li>
                 </ul>
               </div>
@@ -100,7 +101,7 @@
                 <div class="col-sm-12 mb-3">
                   <div class="card h-100">
                     <div class="card-body">
-                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Expertise</i> Status</h6>
+                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Expertise</i></h6>
                       <small>Web Design</small>
                       <div class="progress mb-3" style="height: 5px">
                         <div class="progress-bar bg-warning" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
@@ -117,9 +118,17 @@
                       <div class="progress mb-3" style="height: 5px">
                         <div class="progress-bar bg-warning" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                       </div>
+                      <small>Frameworks Bootstrap</small> 
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-warning" role="progressbar" style="width: 65%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Frameworks Tailwind</small> 
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
                       <small>Backend API</small>
                       <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-warning" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar bg-warning" role="progressbar" style="width: 45%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
                       </div>
                     </div>
                   </div>
@@ -134,5 +143,34 @@
 
         </div>
     </div>
+
+    <div class="gallery">
+    <div class="container-md ">
+      <div class="row pt-5">
+        <h1 class="text-center pb-5 pt-5 h1">Prototipado</h1>
+      </div>
+      <p class="text-center mb-3">Diseño de prototipos y templates con Adobe XD, para <span class="fw-bold">El portal de Formación </span>. Aplicacion responsive</p>
+      <img class="m-5" src="./img/AdobeXD.png" alt="" width="100%"> 
+
+      <div class="row pt-5">
+        <h1 class="text-center pb-5 pt-5 h1">Analisis UX con Maze</h1>
+      </div>
+      <p class="text-center mb-3">Creación de aplicaciones para analisis de rendiemientos UX con <span class="fw-bold">Maze </span>. Antes de maquetar, las cosas 
+    deben probarse, para lograr la mejor interface posible, adecuada a cada necesidad. Impulse la toma de decisiones con pruebas rápidas y remotas</p>
+      <img class="m-5 rounded mx-auto d-block" src="./img/maze.png" alt="" width="40%"> 
+
+      <div class="row pt-5">
+        <h1 class="text-center pb-5 pt-5 h1">Maquetación</h1>
+      </div>
+      <p class="text-center mb-3">Del diseño a la realidad con los Frameworks mas modernos <span class="fw-bold">Bootstrap, Tailwind, Chakra </span> </p>
+      <img class="m-5 rounded mx-auto d-block" src="./img/logoCss.png" alt="" width="70%">
+      
+    </div>
+  </div>
+
+  
+
+
+
 
 <?php include ("footer.php"); ?>
